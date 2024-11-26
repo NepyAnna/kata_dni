ebdc4f4c0fb0c8be964890d6568df6b690d37c92
NepyAnna <nepyann@gmail.com>
2024-11-26 22:33:37 +0100

    edit class InputValidator

--- src/main/java/com/sheoanna/InputValidator.java
+++ src/main/java/com/sheoanna/InputValidator.java
@@ -1,7 +1,24 @@
 package com.sheoanna;
 
 public class InputValidator {
+    public static boolean isNumeric(String input) {
+        if (input == null || input.isEmpty()) {
+            return false;
+        }
 
+        int start = 0;
+        if (input.charAt(0) == '-') {
+            start = 1;
+        }
+
+        for (int i = start; i < input.length(); i++) {
+            if (!Character.isDigit(input.charAt(i))) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
     public static boolean isInRange(int number) {
         return number >= 0 && number <= 99999999;
     }
