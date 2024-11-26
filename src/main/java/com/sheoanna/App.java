7d749ba71be134ba7dce05e239c1e1c551e49c86
NepyAnna <nepyann@gmail.com>
2024-11-26 22:35:31 +0100

    edit APP

--- src/main/java/com/sheoanna/App.java
+++ src/main/java/com/sheoanna/App.java
@@ -10,24 +10,12 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int dniNumber = -1;
-            boolean isValid = false;
+        System.out.println("Welcome to the DNI Calculator!");
+        System.out.println("------------------------------");
 
-            while (!isValid) {
-                System.out.println("Enter your DNI number (0-99999999): ");
-                String input = scanner.nextLine();
-                    if (InputValidator.isInRange(dniNumber)) {
-                        dniNumber = Integer.parseInt(input);
-                        isValid = true;
-                    } else {
-                        System.out.println("Invalid input. Please enter a numeric value.");
-                    }
-                }
-            char dniLetter = DniCalculator.calculateDniLetter(dniNumber);
-            System.out.println("The letter for DNI " + dniNumber + " is: " + dniLetter);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-        }
+        DNIView view = new DNIView();
+        DNIModel model = new DNIModel(0);
+        DNIController controller = new DNIController(model, view);
+
     }
 }
