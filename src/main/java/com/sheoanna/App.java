6d9caf9acbe9b78d0ec51d222be163ea12cb4beb
NepyAnna <nepyann@gmail.com>
2024-11-26 19:55:51 +0100

    implemented APP file

--- src/main/java/com/sheoanna/App.java
+++ src/main/java/com/sheoanna/App.java
@@ -5,13 +5,31 @@ import java.util.Scanner;
 public final class App {
     private App() {
     }
-
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int dniNumber;
+        try (Scanner scanner = new Scanner(System.in)) {
+            int dniNumber = 0;
+            boolean isValid = false;
+
+            while (!isValid) {
+                System.out.println("Enter your DNI number (0-99999999): ");
+                String input = scanner.nextLine();
+                    if (InputValidator.isInRange(dniNumber)) {
+                        dniNumber = Integer.parseInt(input);
+                        isValid = true;
+                    } else {
+                        System.out.println("Invalid input. Please enter a numeric value.");
+                    }
+                }
+
+            char dniLetter = DniCalculator.calculateDniLetter(dniNumber);
+            System.out.println("The letter for DNI " + dniNumber + " is: " + dniLetter);
+        } catch (NumberFormatException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
