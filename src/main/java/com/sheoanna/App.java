7966f0f3dbcb1698e8b44688a28a82e7265f8d9b
NepyAnna <nepyann@gmail.com>
2024-11-26 22:18:10 +0100

    refactoring

--- src/main/java/com/sheoanna/App.java
+++ src/main/java/com/sheoanna/App.java
@@ -5,29 +5,33 @@ import java.util.Scanner;
 public final class App {
     private App() {
     }
+
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            int dniNumber = -1;
-            boolean isValid = false;
+        Scanner scanner = new Scanner(System.in);
+        int dniNumber = -1;
+        boolean isValid = false;
 
-            while (!isValid) {
-                System.out.println("Enter your DNI number (0-99999999): ");
-                String input = scanner.nextLine();
-                    if (InputValidator.isInRange(dniNumber)) {
-                        dniNumber = Integer.parseInt(input);
-                        isValid = true;
-                    } else {
-                        System.out.println("Invalid input. Please enter a numeric value.");
-                    }
+        while (!isValid) {
+            System.out.println("Enter your DNI number (0-99999999): ");
+            String input = scanner.nextLine();
+
+            if (InputValidator.isNumeric(input)) {
+                dniNumber = Integer.parseInt(input);
+                if (InputValidator.isInRange(dniNumber)) {
+                    isValid = true; // Valid input, exit loop
+                } else {
+                    System.out.println("Number is out of range. Please try again.");
                 }
-            char dniLetter = DniCalculator.calculateDniLetter(dniNumber);
-            System.out.println("The letter for DNI " + dniNumber + " is: " + dniLetter);
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
+            } else {
+                System.out.println("Invalid input. Please enter a numeric value.");
+            }
         }
+
+        char dniLetter = DniCalculator.calculateDniLetter(dniNumber);
+        System.out.println("The letter for DNI " + dniNumber + " is: " + dniLetter);
     }
 }
