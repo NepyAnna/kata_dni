417b0440743f1134b23ee07e89968439ef8d1665
NepyAnna <nepyann@gmail.com>
2024-11-26 22:29:06 +0100

    create class DNIController

---
+++ src/main/java/com/sheoanna/DniController.java
@@ -0,0 +1,12 @@
+package com.sheoanna;
+
+public class DNIController {
+    private DNIModel model;
+    private DNIView view;
+
+    public DNIController(DNIModel model, DniView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+}
