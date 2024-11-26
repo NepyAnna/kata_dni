5e29f15dfffb807872ba76ebee8f0fa2d1d10db1
NepyAnna <nepyann@gmail.com>
2024-11-26 22:31:51 +0100

    create class DNIView

--- src/main/java/com/sheoanna/DniController.java
+++ src/main/java/com/sheoanna/DniController.java
@@ -4,7 +4,7 @@ public class DNIController {
     private DNIModel model;
     private DNIView view;
 
-    public DNIController(DNIModel model, DniView view) {
+    public DNIController(DNIModel model, DNIView view) {
         this.model = model;
         this.view = view;
     }
--- src/main/java/com/sheoanna/InputValidator.java
+++ src/main/java/com/sheoanna/InputValidator.java
@@ -1,9 +1,6 @@
 package com.sheoanna;
 
 public class InputValidator {
-   /*  public static int validateInput(String input) {
-        return 0;
-    }*/
 
     public static boolean isInRange(int number) {
         return number >= 0 && number <= 99999999;
