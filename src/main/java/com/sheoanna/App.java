4d0ce8fadeb3872dc820098f56810f20b39dc036
NepyAnna <nepyann@gmail.com>
2024-12-03 12:54:33 +0100

    syntax

--- src/main/java/com/sheoanna/App.java
+++ src/main/java/com/sheoanna/App.java
@@ -9,30 +9,6 @@ public final class App {
      * @param args The arguments of the program.
      */
     public static void main(String[] args) {
-<<<<<<< HEAD
-        Scanner scanner = new Scanner(System.in);
-        int dniNumber = -1;
-        boolean isValid = false;
-
-        while (!isValid) {
-            System.out.println("Enter your DNI number (0-99999999): ");
-            String input = scanner.nextLine();
-
-            if (InputValidator.isNumeric(input)) {
-                dniNumber = Integer.parseInt(input);
-                if (InputValidator.isInRange(dniNumber)) {
-                    isValid = true; // Valid input, exit loop
-                } else {
-                    System.out.println("Number is out of range. Please try again.");
-                }
-            } else {
-                System.out.println("Invalid input. Please enter a numeric value.");
-            }
-        }
-
-        char dniLetter = DniCalculator.calculateDniLetter(dniNumber);
-        System.out.println("The letter for DNI " + dniNumber + " is: " + dniLetter);
-=======
         System.out.println("Welcome to the DNI Calculator!");
         System.out.println("------------------------------");
 
@@ -41,6 +17,5 @@ public final class App {
         DniController controller = new DniController(model, view);
 
         controller.start();
->>>>>>> feature/MVVmC
     }
 }
--- src/main/java/com/sheoanna/InputValidator.java
+++ src/main/java/com/sheoanna/InputValidator.java
@@ -2,17 +2,6 @@ package com.sheoanna;
 
 public class InputValidator {
     public static boolean isNumeric(String input) {
-<<<<<<< HEAD
-    if (input == null || input.isEmpty()) {
-        return false;
-    }
-
-    int start = 0;
-        if (input.charAt(0) == '-') {
-            start = 1;
-        }
-
-=======
         if (input == null || input.isEmpty()) {
             return false;
         }
@@ -20,21 +9,13 @@ public class InputValidator {
         if (input.charAt(0) == '-') {
             start = 1;
         }
->>>>>>> feature/MVVmC
         for (int i = start; i < input.length(); i++) {
             if (!Character.isDigit(input.charAt(i))) {
                 return false;
             }
-<<<<<<< HEAD
-    }
-    return true;
-}
-
-=======
         }
         return true;
     }
->>>>>>> feature/MVVmC
     public static boolean isInRange(int number) {
         return number >= 0 && number <= 99999999;
     }
